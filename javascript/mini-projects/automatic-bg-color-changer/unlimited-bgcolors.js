5c4b0130c5877b11a3f1eb65a2860b67213e4f33
Pranjal Chavan <pracha041105@gmail.com>
2026-06-29 17:09:12 +0530

    Added automatic background color changer using JavaScript

---
+++ javascript/mini-projects/automatic-bg-color-changer/unlimited-bgcolors.js
@@ -0,0 +1,31 @@
+//generating a random background color
+
+const randomColor = function() {
+    const hex = "0123456789ABCDEF"
+    let color = '#'
+    for(let i = 0; i < 6; i++){
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
+
+let intervalId;
+
+const startChangingColor = function () {
+    if(!intervalId) {
+        intervalId = setInterval(changeBgColor, 1000);  //used for continuously changing bg color after 1s
+    }
+
+    function changeBgColor() {
+    document.body.style.backgroundColor = randomColor();
+    }
+};
+
+const stopChangingColor = function () {
+    clearInterval(intervalId);
+    intervalId = null;  //for cleaner code 
+};
+
+document.querySelector("#start").addEventListener('click', startChangingColor)
+
+document.querySelector("#stop").addEventListener('click', stopChangingColor)
